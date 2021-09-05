66f952ebbfe59f75c7dc9f2cdb790a3875304ca6
Bideo Wego <bideowego@gmail.com>
2021-09-05 05:04:23 -0400

    Cleaning up magic numbers and improving jump

--- src/game.ts
+++ src/game.ts
@@ -1,19 +1,32 @@
 import 'phaser';
 
 export default class Home extends Phaser.Scene {
+    readonly IS_COMPLETED = false;
+    readonly NUM_COINS_START = 0;
+    readonly PLAYER_AABB = [30, 30, 2, 2];
+    readonly PLAYER_BOUNCE = 0.001;
+    readonly PLAYER_JUMP_AIR_TIME = 100;
+    readonly PLAYER_JUMP_DOWN_TIME = 0;
+    readonly PLAYER_SPEED_X = 200;
+    readonly PLAYER_SPEED_Y = 300;
+
     map;
     player;
     floor;
     coin;
     positions;
     cursors;
-    isComplete;
+    isCompleted;
     numCoins;
+    playerJumpAirTime;
+    playerJumpDownTime;
 
     constructor() {
         super('home');
-        this.isComplete = false;
-        this.numCoins = 0;
+        this.isCompleted = this.IS_COMPLETED;
+        this.numCoins = this.NUM_COINS_START;
+        this.playerJumpAirTime = this.PLAYER_JUMP_AIR_TIME;
+        this.playerJumpDownTime = this.PLAYER_JUMP_DOWN_TIME;
     }
 
     preload() {
@@ -39,19 +52,19 @@ export default class Home extends Phaser.Scene {
         }, {});
 
         this.player = this.physics.add.sprite(this.positions.startPosition.x, this.positions.startPosition.y, 'player');
-        this.player.setBounce(0.001);
-        this.player.body.setSize(30, 30, 2, 2);
+        this.player.setBounce(this.PLAYER_BOUNCE);
+        this.player.body.setSize(...this.PLAYER_AABB);
         this.physics.add.collider(this.player, this.floor);
         this.physics.add.overlap(this.player, this.coin, this._handleCoin, null, this);
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
         this.cursors.left.on('down', () => {
-            this.player.body.setVelocityX(-200);
+            this.player.body.setVelocityX(-this.PLAYER_SPEED_X);
         });
 
         this.cursors.right.on('down', () => {
-            this.player.body.setVelocityX(200);
+            this.player.body.setVelocityX(this.PLAYER_SPEED_X);
         });
 
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
@@ -60,21 +73,44 @@ export default class Home extends Phaser.Scene {
     }
 
     update(time, delta) {
-        if (!this.isComplete &&
+        if (!this.isCompleted &&
             this.player.x > this.positions.endPosition.x &&
             this.player.x < this.positions.endPosition.x + this.positions.endPosition.width &&
             this.player.y > this.positions.endPosition.y &&
             this.player.y < this.positions.endPosition.y + this.positions.endPosition.height) {
-            this.isComplete = true;
+            this.isCompleted = true;
         }
 
-        if (!this.isComplete) {
+        if (!this.isCompleted) {
+            if (!this.player.body.onFloor()) {
+                if (this.cursors.left.isDown) {
+                    this.player.body.setVelocityX(-this.PLAYER_SPEED_X);
+                }
+
+                if (this.cursors.right.isDown) {
+                    this.player.body.setVelocityX(this.PLAYER_SPEED_X);
+                }
+            }
+
             if (!(this.cursors.left.isDown || this.cursors.right.isDown)) {
                 this.player.body.setVelocityX(0);
             }
 
-            if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
-                this.player.body.setVelocityY(-350);
+            if (this.playerJumpDownTime <= 0) {
+                if ((this.cursors.space.isDown || this.cursors.up.isDown)) {
+                    if (this.player.body.onFloor()) {
+                        this.player.body.setVelocityY(-this.PLAYER_SPEED_Y);
+                        this.playerJumpAirTime = this.PLAYER_JUMP_AIR_TIME;
+                    } else if (this.playerJumpAirTime > 0) {
+                        this.player.body.setVelocityY(-this.PLAYER_SPEED_Y);
+                        this.playerJumpAirTime -= delta;
+                    } else {
+                        this.playerJumpDownTime = this.PLAYER_JUMP_DOWN_TIME;
+                    }
+                }
+            } else if (this.playerJumpDownTime > 0 && this.player.body.onFloor()) {
+                this.playerJumpDownTime -= delta;
+                console.log(this.playerJumpDownTime, delta);
             }
         } else {
             console.log('Level complete!');
@@ -86,7 +122,6 @@ export default class Home extends Phaser.Scene {
             this.map.removeTile(coin);
             coin.destroy(coin.x, coin.y);
             this.numCoins += 1;
-            console.log(this.numCoins);
         }
         return false;
     }
